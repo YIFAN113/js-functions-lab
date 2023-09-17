f10bb19edca42423196f71add45217f2dfa21731
YIFAN113 <20095253@mail.wit.ie>
2023-09-18 03:44:39 +0800

    Exercise 3.

--- exercise3.js
+++ exercise3.js
@@ -1,9 +1,11 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-    const todoTitles = json.map(todo => todo.title);
+    const uncompleted = json
+      .filter(todo => !todo.completed)
+      .map(todo => ({ userID: todo.userId, title: todo.title }));
 
-    console.log(todoTitles)
+    console.log(uncompleted);
   })
   .catch(function(err) { 
     console.log(err);
