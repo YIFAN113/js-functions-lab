51bf91d7de402ac8f1eb41d2ba83844d61ee4cb9
YIFAN113 <20095253@mail.wit.ie>
2023-09-18 04:26:43 +0800

    Exercise 5.

---
+++ exercise5.js
@@ -0,0 +1,15 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+    const uncompleted = json.reduce((accumulator, todo) => {
+      if (!todo.completed) {
+        accumulator.push({ userID: todo.userId, title: todo.title });
+      }
+      return accumulator;
+    }, []);
+
+    console.log(uncompleted);
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
